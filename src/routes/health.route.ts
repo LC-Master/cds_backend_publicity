6d9f38d5338e6bed35031f3c6d1cf02dde2d77c5
Francisco Rojas <franciscoantonior30@gmail.com>
2026-01-19 08:44:43 -0400

    health.route.ts validation was added in route /health

--- src/routes/health.route.ts
+++ src/routes/health.route.ts
@@ -1,9 +1,19 @@
-import Elysia from "elysia";
+import Elysia, { t } from "elysia";
 
-export const healthRoute = new Elysia().get("/health", () => {
-  return {
-    status: "ok",
-    timeStamp: new Date().toISOString(),
-    uptime: Math.round(process.uptime()),
-  };
-});
+export const healthRoute = new Elysia().get(
+  "/health",
+  () => {
+    return {
+      status: "ok",
+      timeStamp: new Date().toISOString(),
+      uptime: Math.round(process.uptime()),
+    };
+  },
+  {
+    response: t.Object({
+      status: t.String(),
+      timeStamp: t.String(),
+      uptime: t.Number(),
+    }),
+  }
+);
